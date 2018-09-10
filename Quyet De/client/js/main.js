0d8ada754bae6c9efe1f066d18eebd5bdf7fffe4
nguyengochie <42274492+nguyengochie@users.noreply.github.com>
2018-09-10 09:15:05 +0700

    Update main.js

--- Quyet De/client/js/main.js
+++ Quyet De/client/js/main.js
@@ -14,7 +14,7 @@ $(document).ready(function () {
     });
 
     $('.answer').on('click', function (e) {
-        
+
         let answer = $(e.target).attr("data-answer");
         let questionId = $(e.target).attr('data-id');
         let dataAnswer = {
@@ -23,7 +23,7 @@ $(document).ready(function () {
         };
         // console.log(dataAnswer.YN, questionId);
         $.ajax({
-            
+
             url: 'http://localhost:6969/answer',
             method: 'PUT',
             // dataType:'html',
@@ -31,11 +31,14 @@ $(document).ready(function () {
             success: function (data) {
                 if (data.question) {
                     let totalVote = data.question.yes + data.question.no;
-                    $('#vote').text(totalVote);
+                    $('#vote').text(totalVote + ' vote');
                     $('#voteYes').text(((data.question.yes / totalVote) * 100).toFixed(2));
                     $('#voteNo').text(((data.question.no / totalVote) * 100).toFixed(2));
                     $('.questionInfo').css('display', 'block');
                     $('.answers').css('display', 'none');
+
+                    $('#result_yes').css('width', ((data.question.yes / totalVote) * 100).toFixed(2) + '%');
+                    $('#result_no').css('width', ((data.question.no / totalVote) * 100).toFixed(2) + '%');
                 }
                 console.log("success!", data);
             },
@@ -54,11 +57,19 @@ $(document).ready(function () {
             success: function (data) {
                 if (data.question) {
                     let totalVote = data.question.yes + data.question.no;
-                    $('#vote').text(totalVote);
+                    $('#vote').text(totalVote + ' vote');
                     $('#voteYes').text(((data.question.yes / totalVote) * 100).toFixed(2));
                     $('#voteNo').text(((data.question.no / totalVote) * 100).toFixed(2));
                     $('.questionInfo').css('display', 'block');
                     $('.answers').css('display', 'none');
+                    if (totalVote == 0) {
+                        $('#result_yes').css('width', '50%');
+                        $('#result_no').css('width', '50%');
+                    } else {
+                        $('#result_yes').css('width', ((data.question.yes / totalVote) * 100).toFixed(2) + '%');
+                        $('#result_no').css('width', ((data.question.no / totalVote) * 100).toFixed(2) + '%');
+                    }
+
                 }
             },
             error: function (error) {
